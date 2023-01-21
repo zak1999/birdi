07218051e642336b04d2dda4eccf61a2530e1850
alexryanjones <alexryanjones@gmx.com>
2023-01-21 15:07:38 +0100

    add: login test & refactored login window to redirect instead of popout

--- client/src/components/Navbar.cy.jsx
+++ client/src/components/Navbar.cy.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import reducers from '../redux/reducers';
 import { BrowserRouter } from 'react-router-dom'
 import { createStore } from 'redux';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 
 describe('<Navbar />', () => {
@@ -13,10 +14,13 @@ describe('<Navbar />', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(
       <Provider store={store}>
-        <BrowserRouter>
-          <Navbar />
-        </BrowserRouter>
+        <Auth0Provider domain={process.env.REACT_APP_AUTH_DOMAIN} clientId={process.env.REACT_APP_AUTH_CLIENT_ID} redirectUri={'http://localhost:8080/__/#/specs/runner?file=src/components/Navbar.cy.jsx'}>
+          <BrowserRouter>
+            <Navbar />
+          </BrowserRouter>
+        </Auth0Provider>
       </Provider>
     )
+    cy.get('[data-testid=login-button]').click()
   })
 })
