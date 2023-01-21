e0175fc8ef5bcdf91e128994f070b921aa2af098
seth <contact.sethjplatt@gmail.com>
2023-01-21 12:23:08 +0100

    test: active card renders information

--- client/src/components/ActiveCard.cy.jsx
+++ client/src/components/ActiveCard.cy.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ActiveCard from './ActiveCard';
 
 describe('<ActiveCard />', () => {
+  const sciName = 'Anser anser';
+  const comName = 'Graylag Goose';
+  const obsDt = '2023-01-20 10:49';
   it('renders on the page', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(
@@ -9,7 +12,7 @@ describe('<ActiveCard />', () => {
         bird={{
           _id: '63c96b3142bbc3cac1747432',
           comName: 'Graylag Goose',
-          sciName: 'anser anser',
+          sciName: 'Anser anser',
           userID: '',
           userEmail: '',
           obsDt: '2023-01-20 10:49',
@@ -19,29 +22,89 @@ describe('<ActiveCard />', () => {
         }}
       />
     );
-    cy.get('Graylag Goose');
   });
 
-  // it('fetches from wikipedia', () => {
-  //   // see: https://on.cypress.io/mounting-react
-  //   cy.mount(
-  //     <ActiveCard
-  //       bird={{
-  //         _id: '63c96b3142bbc3cac1747432',
-  //         comName: 'Graylag Goose',
-  //         sciName: 'anser anser',
-  //         userID: '',
-  //         userEmail: '',
-  //         obsDt: '2023-01-20 10:49',
-  //         url: '',
-  //         lat: '',
-  //         lng: '',
-  //       }}
-  //     />
-  //   );
-  //   cy.intercept('GET', 'https://en.wikipedia.org/*').as('getWikiBird');
-  //   cy.visit(
-  //     'https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=extracts&generator=prefixsearch&redirects=1&converttitles=1&formatversion=2&exintro=1&explaintext=1&gpssearch=$Anser%20anser'
-  //   );
-  // });
+  it('displays the correct scientific name', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(
+      <ActiveCard
+        bird={{
+          _id: '63c96b3142bbc3cac1747432',
+          comName: 'Graylag Goose',
+          sciName: 'Anser anser',
+          userID: '',
+          userEmail: '',
+          obsDt: '2023-01-20 10:49',
+          url: '',
+          lat: '',
+          lng: '',
+        }}
+      />
+    );
+
+    cy.get('[data-testid=active-sci-name]').then(($span) => {
+      // $span is the object that the previous command yielded
+
+      const renderedSciName = $span.text();
+
+      cy.log(renderedSciName);
+      expect(renderedSciName).to.equal(sciName);
+    });
+  });
+
+  it('displays the correct common name', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(
+      <ActiveCard
+        bird={{
+          _id: '63c96b3142bbc3cac1747432',
+          comName: 'Graylag Goose',
+          sciName: 'Anser anser',
+          userID: '',
+          userEmail: '',
+          obsDt: '2023-01-20 10:49',
+          url: '',
+          lat: '',
+          lng: '',
+        }}
+      />
+    );
+
+    cy.get('[data-testid=active-com-name]').then(($span) => {
+      // $span is the object that the previous command yielded
+
+      const renderedComName = $span.text();
+
+      cy.log(renderedComName);
+      expect(renderedComName).to.have.string(comName);
+    });
+  });
+
+  it('displays the correct observed at time', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(
+      <ActiveCard
+        bird={{
+          _id: '63c96b3142bbc3cac1747432',
+          comName: 'Graylag Goose',
+          sciName: 'Anser anser',
+          userID: '',
+          userEmail: '',
+          obsDt: '2023-01-20 10:49',
+          url: '',
+          lat: '',
+          lng: '',
+        }}
+      />
+    );
+
+    cy.get('[data-testid=active-seen-at]').then(($span) => {
+      // $span is the object that the previous command yielded
+
+      const renderedSeenAt = $span.text();
+
+      cy.log(renderedSeenAt);
+      expect(renderedSeenAt).to.have.string(obsDt);
+    });
+  });
 });
