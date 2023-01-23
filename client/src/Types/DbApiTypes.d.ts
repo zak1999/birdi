1b6e9c88f72488a91cde0aa01cb25ccac37679fb
seth <contact.sethjplatt@gmail.com>
2023-01-23 17:46:07 +0100

    refactor: types made for dbFunctions and eBirdApiFunctions

---
+++ client/src/Types/DbApiTypes.d.ts
@@ -0,0 +1,29 @@
+export interface CollectSightingsDBResponse {
+  data: [BirdiUserSighting] | null;
+  error: null | String;
+}
+
+export interface BirdiUserSighting {
+  _id: String;
+  comName: String;
+  sciName: String;
+  userID: String;
+  userEmail: String;
+  url: String;
+  obsDt: String;
+  lat: Number;
+  lng: Number;
+  __v: Number;
+}
+
+export interface UserData {
+  email: String;
+  __v: Number;
+  _id: String;
+  birdSightingsIds: Number[];
+}
+
+export interface SendBirdSightingsToDBResult {
+  result: BirdiUserSighting;
+  userResult: UserData;
+}
---
+++ client/src/Types/EBirdTypes.d.ts
@@ -0,0 +1,16 @@
+export interface EBird {
+  comName: String;
+  howMany: Number;
+  id: Number;
+  lat: Number;
+  lng: Number;
+  locId: String;
+  locName: String;
+  locationPrivate: Boolean;
+  obsDt: String;
+  obsReviewed: Boolean;
+  obsValid: Boolean;
+  sciName: String;
+  speciesCode: String;
+  subId: String;
+}
