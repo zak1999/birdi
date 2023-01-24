4a8a6aba0a21d7f10b64c90a740676338aaf29df
alexryanjones <alexryanjones@gmx.com>
2023-01-24 14:50:57 +0100

    add: active card expand test

--- client/cypress/e2e/ListItem-ActiveCard.cy.js
+++ client/cypress/e2e/ListItem-ActiveCard.cy.js
@@ -2,7 +2,7 @@ describe('active card and list item interaction', () => {
   it('renders the active card on list item click', () => {
     cy.visit('http://localhost:3000');
     cy.findByTestId('active-card-placeholder').should('be.visible');
-    cy.findAllByRole('list-item')
+    cy.findAllByRole('list-item', {timeout: 5000})
       .first()
       .click()
       .then(() => {
