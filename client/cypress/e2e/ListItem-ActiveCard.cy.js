2b16ac318b92f769b5b26053f63a952e1f7a40b7
alexryanjones <alexryanjones@gmx.com>
2023-01-24 10:30:51 +0100

    clean: list item test formatting

--- client/cypress/e2e/ListItem-ActiveCard.cy.js
+++ client/cypress/e2e/ListItem-ActiveCard.cy.js
@@ -4,8 +4,7 @@ describe('active card and list item interaction', () => {
   it('renders the active card on list item click', () => {
     cy.visit('http://localhost:3000');
     cy.findByTestId('active-card-placeholder').should('be.visible');
-    let list = cy
-      .findAllByRole('list-item')
+    cy.findAllByRole('list-item')
       .first()
       .click()
       .then(() => {
