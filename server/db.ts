ca4a4417076855adaebf4233c37c912f82f05783
Zakariyya Ahmed <zakariyya_ahmed@outlook.com>
2023-02-14 19:42:29 +0000

    refactor: db uri points to env

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,19 @@
 // const mongoose = require('mongoose')
-import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+
+const uri = process.env.MONGO_URL //|| `127.0.0.1:27017/birdi`
+
 
 async function connectToDB() {
+  console.log("is this shit on the fly")
   try {
-    await mongoose.connect(`mongodb://127.0.0.1:27017/birdi`)
-    console.log("db connected")
+    mongoose.set('strictQuery', true);
+    await mongoose.connect('mongodb+srv://'+uri);
+    console.log('Successfully connected to the database 🎉');
   } catch (err) {
-    console.log("Error: ",err)
+    console.log('Mongoose connection err: ' + err);
   }
 }
 connectToDB();
