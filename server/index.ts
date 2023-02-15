d8e57541b250eccfa1ede5d412ef088c993415aa
alexryanjones <alexryanjones@gmx.com>
2023-02-15 21:24:32 +0100

    add: cors options

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,24 @@
-import app from './server';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import Express from 'express';
+import cors from 'cors';
+import router from './router';
+
 const port = 3001;
+
+const app = Express();
+
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://birdi.netlify.app/'],
+};
+
+app.use(cors(corsOptions));
+
+app.use(Express.json());
+app.use(router);
 
 app.listen(port, () => {
   console.log('Server listening on port', port);
 });
+
+export default app;
